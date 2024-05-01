ba7fb2bfca5fc0bd894715d16e67a2a65f424f40
leoniealika <168409705+leoniealika@users.noreply.github.com>
2024-05-01 11:47:19 +0200

    transform.php

--- transform.php
+++ transform.php
@@ -1,11 +1,8 @@
 <?php
 include 'config.php'; 
 
-// Aktuelles Datum generieren
-$currentDate = date('Y-m-d');
-
 // Hier erfolgt der API-Aufruf und die Datenextraktion -- DYNAMISCHER LINK MÖGLICH???
-$apiData = file_get_contents("https://il.srgssr.ch/integrationlayer/2.0/srf/songList/radio/byChannel/69e8ac16-4327-4af4-b873-fd5cd6e895a7?from={$currentDate}T00%3A00%3A00%2B02%3A00&to={$currentDate}T23%3A59%3A00%2B02%3A00&pageSize=500");
+$apiData = file_get_contents("https://il.srgssr.ch/integrationlayer/2.0/srf/songList/radio/byChannel/69e8ac16-4327-4af4-b873-fd5cd6e895a7");
 
 // Die Daten werden in ein Array konvertiert
 $data = json_decode($apiData, true);
