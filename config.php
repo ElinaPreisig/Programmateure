1d6a4783446adab125fcdaebd235f43ff0d7339f
Elina <146704471+ElinaPreisig@users.noreply.github.com>
2024-05-26 16:43:21 +0200

    Update config.php for public respository

--- config.php
+++ config.php
@@ -4,14 +4,14 @@
 $db_host     = 'localhost';     // Hostserver, auf dem die DB läuft.
 // «localhost» bedeutet: die selbe Serveradresse, auf dem auch die Seiten gespeichert sind
 
-$db_name = '148951_4_1';   // Name der Datenbank (stimmt im Beispiel nur zufällig mit username überein)
-$db_user = '148951_4_1';   // Name des DB-Users (stimmt im Beispiel nur zufällig mit Datenbankname überein)
-$db_pass = 'yNJgZQHWB2oe';  // Passwort des DB-Users*/
+//$db_name = wegen öffentlichem Git Respository entfernt
+//$db_user = wegen öffentlichem Git Respository entfernt 
+//$db_pass = wegen öffentlichem Git Respository entfernt
 
-$db_charset  = 'utf8mb4';       // siehe https://www.hydroxi.de/utf8-vs-utf8mb4/
+$db_charset  = 'utf8mb4';       
 
 // DSN (Datenquellenname) für PDO
-$dsn = "mysql:host=$db_host;dbname=$db_name;charset=$db_charset"; // siehe https://en.wikipedia.org/wiki/Data_source_name
+$dsn = "mysql:host=$db_host;dbname=$db_name;charset=$db_charset"; 
 
 // Optionen für PDO
 $options = [
